67e0ad86dbea8d054b667f40c1e4390c1139b10b
asurion <asurion61@gmail.com>
2018-01-14 17:33:37 +0100

    Modification page Accueil

--- pages/accueil.php
+++ pages/accueil.php
@@ -1,5 +1,7 @@
 <!-- Main component for a primary marketing message or call to action -->
 <div class="jumbotron">
-    <h1>Navbar example</h1>
-    <p>This example is a quick exercise to illustrate how the default, static navbar and fixed to top navbar work. It includes the responsive CSS and HTML, so it also adapts to your viewport and device.</p>
+    <h1>Bienvenue</h1>
+    <p>Ce site a pour but d'améliorer vos connaissances sur les différentes matières étudiées à l'UFR Science de Caen.<br></p>
+
+    <p style="font-size: small">Ce projet est un projet annuel développé par Lounis Bibi et Nathan Chevalier pendant l'année de L3 Informatique 2017-2018</p>
 </div>
