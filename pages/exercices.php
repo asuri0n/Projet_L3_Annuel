7e2beba867670a631d92d990652b716c57404113
asurion <asurion61@gmail.com>
2018-01-15 16:20:20 +0100

    Page exercice tjrs en cours !

--- pages/exercices.php
+++ pages/exercices.php
@@ -12,6 +12,7 @@
     //
     // vérifications a chaque nouvelle affichage d'une question
     //
+    $end = false;
     if(isset($_POST['next'])){
         if(isset($_POST['answers']) and isset($_POST['qnumber']) and !empty($_POST['answers'] and !empty($_POST['qnumber']))){
             $nbQuestions = $_POST['qnumber'];
@@ -19,7 +20,7 @@
             if(isset($_POST['quizz']) and is_numeric($_POST['quizz'])){
                 // On rajoute la réponse dans la chaine
                 foreach ($answers as $key => $answer) {
-                    if($answer == '0') {
+                    if($answer == '-') {
                         $answers[$key] = $_POST['quizz'];
                         break;
                     }
@@ -32,10 +33,13 @@
             if(sizeof($answers) == $nbQuestions) {
                 $cpt = 0;
                 foreach ($answers as $answer) {
-                    if($answer != '0')
+                    if($answer != '-')
                         $cpt++;
                 }
-                $nextQuestion = $cpt+1;
+                if($cpt == $nbQuestions)
+                    $end = true;
+                else
+                    $nextQuestion = $cpt+1;
             } else {
                 echo "Erreur nombre de questions et nombres de réponses";
             }
@@ -47,69 +51,54 @@
         $nextQuestion = 1;
         $answers = "";
         for($i = 0; $i < $nbQuestions; $i++)
-            $answers .= "0";
+            $answers .= "-";
         $answers = str_split($answers);
     }
 
-    $question = getArrayFrom($pdo, "SELECT id_question, question, type_question.libelle as typelib, choix, reponses FROM questions JOIN type_question USING (id_type) WHERE id_exercice = ".$exercice_id." ORDER BY id_question ASC LIMIT ".($nextQuestion-1).", 1 ", "fetch");
-
-
-
-
-
-    $exercice = getArrayFrom($pdo, "SELECT libelle FROM exercice WHERE id_exercice = ".$exercice_id." LIMIT 1", "fetch");
-
-    $choix = explode(",",$question["choix"]);
+    if(!$end)
+        $question = getArrayFrom($pdo, "SELECT id_question, question, type_question.libelle as typelib, choix, reponses FROM questions JOIN type_question USING (id_type) WHERE id_exercice = ".$exercice_id." ORDER BY id_question ASC LIMIT ".($nextQuestion-1).", 1 ", "fetch");
 
     ob_start();
-    echo "<p style='margin-bottom:30px;'>$nextQuestion. ".$question['question']."</p>";
-    echo "<form role='form' name='quizform' action='".WEBROOT."exercices/".$exercice_id."/1' method='post'>";
-    echo "<input name='starttime' value='1/15/2018 3:09:04 AM' type='hidden'>";
-    echo "<input name='answers' value='".implode($answers)."' type='hidden'>";
-    echo "<input name='qnumber' value='$nbQuestions' type='hidden'>";
-        foreach ($choix as $key => $choi){
+    if(!$end) {
+        $exercice = getArrayFrom($pdo, "SELECT libelle FROM exercice WHERE id_exercice = ".$exercice_id." LIMIT 1", "fetch");
+        $choix = explode(",",$question["choix"]);
+        echo "<h1>".$exercice['libelle']."</h1>
+            <p style='margin-bottom:30px;'>$nextQuestion. " . $question['question'] . "</p>
+            <form role='form' name='quizform' action='" . WEBROOT . "exercices/" . $exercice_id . "/1' method='post'>
+                <input name='starttime' value='1/15/2018 3:09:04 AM' type='hidden'>
+                <input name='answers' value='" . implode($answers) . "' type='hidden'>
+                <input name='qnumber' value='$nbQuestions' type='hidden'>";
+        foreach ($choix as $key => $choi) {
             echo "<div class='radio'><label><input name='quizz' value='$key' type='radio'> $choi</label></div>";
         }
-    echo "<br>";
-    echo "<input name='next' value=' Next ' type='submit'>";
-    echo "</form>";
+                echo "<br>
+                <input name='next' value=' Suivant ' type='submit'>
+            </form>
+            <div>Total $nbQuestions questions</div>";
+
+    } else {
+
+        $nbBonnesReponses = nbBonnesReponses();
+
+        echo "<center><h2>Resultat:</h2>$nbBonnesReponses of $nbQuestions<p><b>8%</b></p><p>You must study much harder!</p><p><b>Time Spent</b><br>0:27</p></center>
+        <form role='form' target='_blank' action='result.asp' method='post'>
+            <input name='points' value='2' type='hidden'>
+            <input name='percentPoints' value='8' type='hidden'>
+            <input name='qtest' value='CSS' type='hidden'>
+            <input name='timespent' value='0:27' type='hidden'>
+            <input name='answers' value='".implode($answers)."' type='hidden'>
+    
+            <table width='100%'><tbody><tr>
+                    <td><input value='Vérifier les réponses' type='submit'></td>
+                    <td align='right'><input value='Try again' onclick='window.location.href = window.location.href;' type='button'></td>
+                </tr></tbody></table>
+        </form>";
+    }
     $content = ob_get_contents();
     ob_end_clean();
 ?>
 
-
-
-
-<div class="w3-col l10 m12" id="main">
-    <h1><?php echo $exercice['libelle'] ?></h1>
-    <?php echo $content ?>
-    <div class="w3-padding-16">
-        <div class="w3-row">
-            <div class="w3-col s6">Total 25 questions</div>
-        </div>
-    </div>
-
-</div>
-
-<br>
-<hr>
-<br>
-
-<div class="w3-padding w3-light-grey w3-large">
-    <center><h2>Result:</h2>2 of 25<p><b>8%</b></p><p>You must study much harder!</p><p><b>Time Spent</b><br>0:27</p></center>
-    <form role="form" target="_blank" action="result.asp" method="post">
-        <input name="points" value="2" type="hidden">
-        <input name="percentPoints" value="8" type="hidden">
-        <input name="qtest" value="CSS" type="hidden">
-        <input name="timespent" value="0:27" type="hidden">
-        <input name="answers" value="1311124132100000000000000" type="hidden">
-
-        <table width="100%"><tbody><tr>
-                <td><input class="w3-btn w3-orange w3-text-white w3-large" value="Check your answers" type="submit"></td>
-                <td align="right"><input class="w3-btn w3-orange w3-text-white w3-large" value="Try again" onclick="location='quiztest.asp?qtest=CSS'" type="button"></td>
-            </tr></tbody></table>
-    </form>
-</div>
+<?php echo $content ?>
 
 <br>
 <hr>
