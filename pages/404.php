70d7d9181a76ef10174b6ef7406fbc8aea9a5e0b
asurion <asurion61@gmail.com>
2018-01-15 08:33:13 +0100

    404 Page

--- pages/404.php
+++ pages/404.php
@@ -1,1 +1,25 @@
-[404 ERROR] La page n'existe pas/plus
+<style type="text/css">
+    .error-template {padding: 40px 15px;text-align: center;}
+    .error-actions {margin-top:15px;margin-bottom:15px;}
+    .error-actions .btn { margin-right:10px; }
+</style>
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="error-template">
+                <h1>
+                    Oops!</h1>
+                <h2>
+                    404 Not Found</h2>
+                <div class="error-details">
+                    Désolé, une erreur est survenue, la page demandée n'a pas été trouvée!
+                </div>
+                <div class="error-actions">
+                    <a href="accueil" class="btn btn-primary btn-lg"><span class="glyphicon glyphicon-home"></span> Retour à l'accueil </a>
+                    <a href="mailto:asurion61@gmail.com" class="btn btn-default btn-lg"><span class="glyphicon glyphicon-envelope"></span> Contacter le support </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
