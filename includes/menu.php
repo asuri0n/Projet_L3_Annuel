761ae8ff533cb098659ce26862d08c7f2853a04b
asurion <asurion61@gmail.com>
2018-03-03 18:58:14 +0100

    Menu Exercice Style submenu

--- includes/menu.php
+++ includes/menu.php
@@ -13,18 +13,25 @@
         <div id="navbar" class="navbar-collapse collapse">
             <ul class="nav navbar-nav">
                 <li <?php if ($params[0] == 'accueil') {echo 'class="active"';} ?> ><a href="<?php echo WEBROOT ?>accueil/">Accueil</a></li>
-                <li <?php if ($params[0] == 'exercices' or $params[0] == 'resultat') {echo 'class="active"';} ?> ><a href="<?php echo WEBROOT ?>exercices">Exercices</a></li>
-
-                <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+                <li <?php if ($params[0] == 'exercices' or $params[0] == 'resultat') {echo 'class="active"';} ?> class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Exercices <span class="caret"></span></a>
                     <ul class="dropdown-menu">
-                        <li><a href="#">Action</a></li>
-                        <li><a href="#">Another action</a></li>
-                        <li><a href="#">Something else here</a></li>
-                        <li role="separator" class="divider"></li>
-                        <li class="dropdown-header">Nav header</li>
-                        <li><a href="#">Separated link</a></li>
-                        <li><a href="#">One more separated link</a></li>
+                        <li class="dropdown-submenu">
+                            <a href="#">Informatique <span class="caret caret-right"></span></a>
+                            <ul class="dropdown-menu">
+                                <li><a href="#">Licence 1</a></li>
+                                <li><a href="#">Licence 2</a></li>
+                                <li><a href="#">Licence 3</a></li>
+                            </ul>
+                        </li>
+                        <li class="dropdown-submenu">
+                            <a href="#">Mathématique <span class="caret caret-right"></span></a>
+                            <ul class="dropdown-menu">
+                                <li><a href="#">Licence 1</a></li>
+                                <li><a href="#">Licence 2</a></li>
+                                <li><a href="#">Licence 3</a></li>
+                            </ul>
+                        </li>
                     </ul>
                 </li>
             </ul>
